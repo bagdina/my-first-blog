f0a22dad1f70e8c06d82f550de5aa1d8894550b8
bagdina <bagdina98@gmail.com>
2018-12-15 16:22:55 +0600

    with api

--- mysite/settings.py
+++ mysite/settings.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
                   'django.contrib.messages',
                   'django.contrib.staticfiles',
                   'blog',
+                  'rest_framework',
                   ]
 
 MIDDLEWARE = [
@@ -116,3 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
+}
